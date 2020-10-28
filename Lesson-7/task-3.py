64eaec8cf55030877b052a0a29f405558885e03d
niko <isp06@mail.ru>
2020-10-28 23:36:01 +0300

    up_task-3

--- Lesson-7/task-3.py
+++ Lesson-7/task-3.py
@@ -14,8 +14,25 @@ from statistics import median
 
 import  numpy as np
 
-a = np.arange(10).reshape(1, 10)
+a = np.arange(22).reshape(1, 22)
 print(a)
 print(np.median(a,axis=(0,1)))
 
-#Вариант решения нашел в библиотеки numpy очень полезная библиотека 
+#Вариант решения нашел в библиотеки numpy очень полезная библиотека
+
+#Вариант решения через ШЕЛЛ
+def shell(seq):
+    inc = len(seq) // 2
+    while inc:
+        for i, el in enumerate(seq):
+            while i >= inc and seq[i - inc] > el:
+                seq[i] = seq[i - inc]
+                i -= inc
+            seq[i] = el
+        inc = 1 if inc == 2 else int(inc * 5.0 / 11)
+
+
+data = [i for i in range(1,22)]
+shell(data)
+print(data)
+print(len(data)/2)
